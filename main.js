c27371c6bb5a4b6e7c8447148973dc011be7ff0b
hector.gv00 <hector.gv00@gmail.com>
2022-08-16 18:55:10 +0200

    cambio en todo. Aplicada funcion enviar texto con fecha y hora

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+function desplegable(){
+
+ let lista = document.getElementsByClassName("desplegable")[0];
+
+   if(lista.style.display = "none"){
+    lista.style.display = "flex";
+}   else{
+    lista.style.removeProperty("display");
+   }
+
+}
+
+function enviar(){
+    let screen = document.getElementById("screen");
+
+    var d = new Date();
+
+    let texto = document.getElementById("input-input-texto").value;
+
+    screen.innerHTML += "<div>" + "<p>" + texto + "</p>" + "<p class='fecha-hora'>" + d.getHours() + ":" + d.getMinutes() + "<br>" + d.toLocaleDateString() + "</p >" 
++    "</div>";
+
+    // screen.innerHTML += "<p>" + d.getHours() + ":" + d.getMinutes() + "<br>" + d.toLocaleDateString() + "</p>" 
+
+    document.getElementById("input-input-texto").value = " ";
+
+
+
+
+}
+
+document.addEventListener('keydown', function(event) {
+    if(event.key == 'Enter') {
+       enviar();
+    }
+});
