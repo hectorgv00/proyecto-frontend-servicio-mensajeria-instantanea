f7870bcbb1922cdf8831d825b77f3ae00ee6e60d
hector.gv00 <hector.gv00@gmail.com>
2022-08-17 11:29:49 +0200

    cambios en el input de texto, ya no ennvía mensajes vacíos

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
 
-
+let archivoMensajes = [];
 
 
 function enviar(){
     let screen = document.getElementById("screen");
-
     var d = new Date();
-
     let texto = document.getElementById("input-input-texto").value;
+
+
+    if(texto === " "){
+        document.getElementById("input-input-texto").value = " ";
+    }else{
 
     screen.innerHTML += "<div>" + "<p>" + texto + "</p>" + "<p class='fecha-hora'>" + d.getHours() + ":" + d.getMinutes() + "<br>" + d.toLocaleDateString() + "</p >" 
 +    "</div>";
 
-    // screen.innerHTML += "<p>" + d.getHours() + ":" + d.getMinutes() + "<br>" + d.toLocaleDateString() + "</p>" 
-
+    archivoMensajes.push(texto);    
+}
     document.getElementById("input-input-texto").value = " ";
 
 
-
+    console.log(archivoMensajes);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.addEventListener('keydown', function(event) {
     if(event.key == 'Enter') {
